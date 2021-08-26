f9edc693b6b0eb77173260bf73c700162e012c41
Patil-Dipak <dips281999@gmail.com>
2021-08-26 23:32:53 +0530

    register the viewset in urls of api

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
+from . views import MovieViewSet, RatingViewSet
 
 router = routers.DefaultRouter()
+# Register the viewsets
+router.register('movies', MovieViewSet)
+router.register('Ratings', RatingViewSet)
 
 urlpatterns = [
      path('', include(router.urls)),
