80f3343e2b6f19f6024c8e8d702bd594c5bea5d0
Patil-Dipak <dips281999@gmail.com>
2021-08-26 22:59:48 +0530

    Added the Movie and Rating class in models file of api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import User     # for User db
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+# Movie table
+class Movie(models.Model):
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+
+#Rating table
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete= models.CASCADE)    # ForeignKey of Movie table
+    user = models.ForeignKey(User, on_delete= models.CASCADE)      #ForeignKey of User table
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = (('user','movie'),)
+        index_together = (('user','movie'),)
