68e8abd8f2bff3feab0cd32c7350fd3c097c8624
Patil-Dipak <dips281999@gmail.com>
2021-08-27 15:05:13 +0530

    Added the rate_movie methods in movieviewet i views of api

--- api/views.py
+++ api/views.py
@@ -1,12 +1,19 @@
 from api.models import Movie
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from . models import Movie, Rating
 from . serializers import MovieSerializer, RatingSerializer
+from rest_framework.decorators import action
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+
+    #Rate the movie
+    @action(detail = True, methods = ['POST'])
+    def rate_movie(self, request, pk = None):
+        response = {'message': 'its working'}
+        return Response(response, status = status.HTTP_200_OK)
 
 
 class RatingViewSet(viewsets.ModelViewSet):
