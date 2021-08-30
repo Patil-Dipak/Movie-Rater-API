cc05896c503ea7e90cbafd041611f7b92baf60f2
Patil-Dipak <dips281999@gmail.com>
2021-08-30 17:32:31 +0530

    Added the permission classes in views of api app

--- api/views.py
+++ api/views.py
@@ -6,6 +6,7 @@ from . serializers import MovieSerializer, RatingSerializer, UserSerializer
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -17,6 +18,7 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
     authentication_classes = (TokenAuthentication, )
+    permission_classes = (IsAuthenticated,)     # permission class
 
     #Rate the movie
     @action(detail = True, methods = ['POST'])
